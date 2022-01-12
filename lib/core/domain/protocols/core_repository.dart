dd7e3178ec46685f14f41df0a979bbf2480c7bdd
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-12 19:43:02 -0300

    feature: adding core repository abstract class

---
+++ lib/core/domain/protocols/core_repository.dart
@@ -0,0 +1,12 @@
+import 'dart:ffi';
+
+import 'package:fpdart/fpdart.dart';
+import 'package:plus_movies/core/domain/errors/core_error.dart';
+
+abstract class CoreRepository<T> {
+  Either<CoreError, T> create(T entity);
+  Either<CoreError, T> update(T entity);
+  Either<CoreError, String> delete(T entity);
+  Either<CoreError, T> find(T entity);
+  Either<CoreError, List<T>> findAll();
+}
