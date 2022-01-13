b18edab58e2660825bbd6674a408ecaeed135d6b
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-13 17:28:31 -0300

    change:  removing core usecase

--- lib/core/domain/usecases/core_usecase.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-
-import 'package:plus_movies/core/domain/errors/core_error.dart';
-
-abstract class CoreUsecase {
-  Future<Either<CoreError, dynamic>> call();
-}
