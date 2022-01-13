785d938a34269e08c8c97f70c51fc0bf49793ab2
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-13 08:39:42 -0300

    feature: adding move professional entity

---
+++ lib/modules/movies/domain/entities/professional.entity.dart
@@ -0,0 +1,11 @@
+class Professional {
+  final int id;
+  final String name;
+  final String department;
+
+  Professional({
+    required this.id,
+    required this.name,
+    required this.department,
+  });
+}
