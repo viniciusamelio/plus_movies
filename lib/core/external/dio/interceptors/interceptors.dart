0d2df68097fbb8b9bfe16546b5201d41dbe16ea6
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-15 12:50:06 -0300

    feature: adding dio movie db token interceptor

---
+++ lib/core/external/dio/interceptors/interceptors.dart
@@ -0,0 +1,1 @@
+export "./movie_db_token_interceptor.dart";
---
+++ lib/core/external/dio/interceptors/movie_db_token_interceptor.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+
+import 'package:plus_movies/env.dart';
+
+final movieDBTokenInterceptor =
+    InterceptorsWrapper(onRequest: (options, handlers) {
+  if (options.path.contains(movieDBBaseUrl)) {
+    options.path += "?api_key=$movieDBKey";
+  }
+});
