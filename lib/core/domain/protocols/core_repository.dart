d6cb522f0fed5d50f60424e3aa938f654c83a736
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-15 14:16:32 -0300

    fix: removing unnecessary methods

--- lib/core/domain/protocols/core_repository.dart
+++ lib/core/domain/protocols/core_repository.dart
@@ -3,8 +3,6 @@ import 'package:plus_movies/core/domain/errors/core_error.dart';
 
 abstract class CoreRepository<T> {
   Future<Either<CoreError, T>> create(T entity);
-  Future<Either<CoreError, T>> update(T entity);
-  Future<Either<CoreError, String>> delete(T entity);
   Future<Either<CoreError, T>> find(String id);
   Future<Either<CoreError, List<T>>> findAll();
 }
