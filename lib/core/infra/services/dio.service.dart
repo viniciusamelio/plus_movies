98acc8d33c1a3eb8ca325b86c91d66dc4def828e
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-15 12:58:48 -0300

    feature: adding Dio Service

---
+++ lib/core/infra/services/dio.service.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:plus_movies/core/external/dio/dio.dart';
+import 'package:plus_movies/core/infra/services/http.service.dart';
+
+class DioService implements HttpService {
+  final CustomDio _dio;
+
+  DioService(this._dio);
+
+  @override
+  Future<dynamic> get(String path, {Map<String, dynamic>? options}) async {
+    try {
+      final result = await _dio.get(
+        path,
+        options: Options(
+          extra: options?["extra"],
+          headers: options?["headers"],
+        ),
+      );
+      return result.data;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
