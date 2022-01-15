97259a675ecb009a10bcd8a6a1f59fe8673a2c12
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-15 13:17:29 -0300

    feature: adding language param to moviedb token interceptor

--- lib/core/external/dio/interceptors/movie_db_token_interceptor.dart
+++ lib/core/external/dio/interceptors/movie_db_token_interceptor.dart
@@ -5,6 +5,6 @@ import 'package:plus_movies/env.dart';
 final movieDBTokenInterceptor =
     InterceptorsWrapper(onRequest: (options, handlers) {
   if (options.path.contains(movieDBBaseUrl)) {
-    options.path += "?api_key=$movieDBKey";
+    options.path += "?api_key=$movieDBKey&language=pt-BR";
   }
 });
