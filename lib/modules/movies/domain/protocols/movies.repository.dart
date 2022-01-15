09e1c36f5d9d378c2d33954d5efa03063bbd4ee6
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-15 09:22:48 -0300

    feature: createAll method to movies repository

--- lib/modules/movies/domain/protocols/movies.repository.dart
+++ lib/modules/movies/domain/protocols/movies.repository.dart
@@ -5,6 +5,5 @@ import 'package:plus_movies/core/domain/protocols/core_repository.dart';
 import 'package:plus_movies/modules/movies/domain/entities/movie.entity.dart';
 
 abstract class MoviesRepository extends CoreRepository<Movie> {
-  @override
-  Future<Either<CoreError, Movie>> create(Movie entity);
+  Future<Either<CoreError, List<Movie>>> createAll(List<Movie> data);
 }
