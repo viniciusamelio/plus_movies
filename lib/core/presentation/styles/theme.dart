40cd5a7c67beb1bc178c231057800563c20cdfc2
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-16 01:27:32 -0300

    style: adding inital theme

---
+++ lib/core/presentation/styles/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:plus_movies/core/presentation/styles/colors.dart';
+
+final theme = ThemeData(
+  backgroundColor: Colors.white,
+  fontFamily: "Montserrat",
+  colorScheme: const ColorScheme(
+    background: bgLight,
+    secondary: darkGreen01,
+    secondaryVariant: darkGreen01,
+    primary: bgLight,
+    primaryVariant: bgLight,
+    surface: gray08,
+    error: Colors.red,
+    onPrimary: gray01,
+    onSecondary: Colors.white,
+    onSurface: gray01,
+    onBackground: gray02,
+    onError: Colors.white,
+    brightness: Brightness.light,
+  ),
+);
