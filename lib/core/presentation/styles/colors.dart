5dc5621d91e0f897718f18b00196e9537d594e84
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-16 01:27:11 -0300

    style: adding layout colors constants

---
+++ lib/core/presentation/styles/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+
+const Color bgLight = Color(
+  0XFFF5F5F5,
+);
+
+const Color darkGreen01 = Color(
+  0XFF00384C,
+);
+
+const Color gray02 = Color(
+  0XFF5E6770,
+);
+
+const Color gray01 = Color(
+  0XFF343A40,
+);
+
+const Color gray08 = Color(
+  0XFFF1F3F5,
+);
