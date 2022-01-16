9cef9fb4078ecf0b0b5ebf441c411342ebf98c3f
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-16 14:09:40 -0300

    feature: adding usecase to movie list presenter

--- lib/modules/movies/presentation/presenters/movie_list.presenter.dart
+++ lib/modules/movies/presentation/presenters/movie_list.presenter.dart
@@ -1,5 +1,10 @@
+import 'package:plus_movies/modules/movies/domain/usecases/list_movies.usecase.dart';
+
 abstract class MovieListPresenter {
+  MovieListPresenter(this.listMoviesUsecase);
   int selectedGenreIndex = 0;
   int maxIndex = 3;
   void selectGenre(int index);
+  final ListMoviesUsecase listMoviesUsecase;
+  dynamic listMovies();
 }
