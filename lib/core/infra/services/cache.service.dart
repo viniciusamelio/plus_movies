cd104896bf35a3cb3871177e32e7d182f83791ee
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-15 21:58:44 -0300

    fix: cache service method signatures

--- lib/core/infra/services/cache.service.dart
+++ lib/core/infra/services/cache.service.dart
@@ -1,6 +1,6 @@
 abstract class CacheService {
   void save(String key, dynamic value);
   void delete(String key);
-  void find(String key, dynamic value);
-  void findAll(String key, dynamic value);
+  find(String key, dynamic value);
+  findAll();
 }
