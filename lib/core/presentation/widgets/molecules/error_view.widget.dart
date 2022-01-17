23716481acb59ec061806c72f2212b0b451d0bdc
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-01-17 08:43:53 -0300

    fix: wrapping error view with singlechild scroll view to avoid overflows

--- lib/core/presentation/widgets/molecules/error_view.widget.dart
+++ lib/core/presentation/widgets/molecules/error_view.widget.dart
@@ -10,34 +10,36 @@ class ErrorViewMolecule extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      height: MediaQuery.of(context).size.height * .6,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/images/movie-error.png",
-              height: 200,
-            ),
-            const Text(
-              "Oopss..",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-                color: gray01,
+    return SingleChildScrollView(
+      child: Container(
+        alignment: Alignment.center,
+        height: MediaQuery.of(context).size.height * .6,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/movie-error.png",
+                height: 200,
               ),
-            ),
-            Text(
-              message,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 16,
-                color: gray02,
+              const Text(
+                "Oopss..",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: gray01,
+                ),
               ),
-            ),
-          ],
+              Text(
+                message,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: gray02,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
